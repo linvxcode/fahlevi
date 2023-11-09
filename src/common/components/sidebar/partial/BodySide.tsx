2351f20173cd9f8a3659449db86ed1047ab5010b
linxcode <linxmusic.com>
2023-11-09 14:55:28 +0700

    fix(headernav):change text

--- src/common/components/sidebar/partial/BodySide.tsx
+++ src/common/components/sidebar/partial/BodySide.tsx
@@ -4,14 +4,14 @@ import VerifiedSvg from '../../svg/VerifiedSvg'
 const BodySide = () => {
   return (
     <div className='flex  flex-col mt-5 '>
-      <h1 className='text-xl font-medium flex items-center gap-2'>
+      <h1 className='text-xl font-medium flex items-center gap-1'>
         Fahlevi
         <span>
           <VerifiedSvg/>
         </span>
       </h1>
-      <h2>
-        @vii
+      <h2 className='text-sm text-neutral-300 lg:hover:text-neutral-400 lg:transition-all lg:duration-300'>
+        @vii_999_
       </h2>
     </div>
   )
